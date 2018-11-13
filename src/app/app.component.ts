4d3e3abde633296d8b85218a40126a6dcbf6cd7b
TirandoCodigoF <virgo_santiago@live.com.mx>
2018-11-13 10:03:25 -0600

    iniciando con el acceso al sistema login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myjob';
+  title = 'MyJoB.COM';
+  title2= 'BIENVENIDO';
 }
