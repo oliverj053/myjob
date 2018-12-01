21c18a58c35cb2b36983eff01caf1251920cb6fb
TirandoCodigoF <virgo_santiago@live.com.mx>
2018-12-01 15:35:04 -0600

    nuevas pruebas en el acceso
    se prohiben

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {HomeComponent} from "./home/home.component";
+import {HomeComponent} from "./Usuario/home/home.component";
+//import {NavbarComponent} from "./Usuario/navbar/navbar.component";
+import {PrivadoComponent} from "./Usuario/privado/privado.component";
 import {LoginUComponent} from "./Usuario/login-u/login-u.component";
 import {RegistroUsComponent} from "./Usuario/registro-us/registro-us.component";
+import {NotFoundComponent} from "./Usuario/not-found/not-found.component";
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path: 'login',component:LoginUComponent},
-  {path: 'registrou', component:RegistroUsComponent}
+  {path: 'registrou', component:RegistroUsComponent},
+  {path: 'privado',component:PrivadoComponent},
+  {path:'**',component:NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,10 @@ import { from } from 'rxjs';
 import {FormsModule} from '@angular/forms';
 import { LoginUComponent } from './Usuario/login-u/login-u.component';
 import { RegistroUsComponent } from './Usuario/registro-us/registro-us.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PrivadoComponent } from './privado/privado.component';
-import { NotFoundComponent } from './not-found/not-found.component'
+import { HomeComponent } from './Usuario/home/home.component';
+import { NavbarComponent } from './Usuario/navbar/navbar.component';
+import { PrivadoComponent } from './Usuario/privado/privado.component';
+import { NotFoundComponent } from './Usuario/not-found/not-found.component'
 
 //arreglo que va a contener los enlaces a mostrar
 const appRoutes:Routes=[
@@ -27,7 +27,9 @@ const appRoutes:Routes=[
   {path:'login-e',component:LoginEComponent},
   {path:'registro', component:RegistroComponent},
   {path:'login-u', component:LoginUComponent},
-  {path:'registro-us', component:RegistroUsComponent}
+  {path:'registro-us', component:RegistroUsComponent},
+  //{path: '**',component:NotFoundComponent}
+
 ];
 
 @NgModule({
