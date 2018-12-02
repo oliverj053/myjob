a9309385f92fd8704c40c9cc36db39f89c10585d
oliverj053 <oliver_js597@outlook.com>
2018-12-02 14:28:03 -0600

    Revert "Revert "login funcional""
    This reverts commit b7dc22db38f73d12a58f2c609b311916ef760b29.

--- src/app/empresa/login-e/login-e.component.ts
+++ src/app/empresa/login-e/login-e.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import{ServicioempService} from '../servicios/servicioemp.service';
+import{Empresa} from '../modelos/empresa';
+import { Key } from 'protractor';
+import { Router } from '@angular/router';
+import { Command } from 'selenium-webdriver';
 @Component({
   selector: 'app-login-e',
   templateUrl: './login-e.component.html',
@@ -8,12 +13,48 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginEComponent implements OnInit {
 
-  constructor() { }
+  empresaList:Empresa[];
+  resultado:any[];
+  constructor(private servicioempService:ServicioempService,private router:Router) { }
 
   ngOnInit() {
+   /* this.servicioempService.getProducts()
+    .snapshotChanges() //meto para obtener todos los cambios de la base de datos
+    .subscribe(item=>{ //nos trae los datos guardados en la bd
+      this.empresaList=[];
+      item.forEach(element=>{
+       let x= element.payload.toJSON();
+        x["nombreU"]=element.key;
+        x["contrasenia"]=element.key
+        this.empresaList.push(x as Empresa);
+      });
+    });*/
+    this.servicioempService.getProducts().
+    valueChanges().subscribe(
+      empresa=>{
+        this.empresaList=empresa;
+      }
+    );
+  
   }
  
   login(form:NgForm){
-      console.log(form.value);
+     //console.log(form.value);
+     //console.log(this.empresaList);
+      this.resultado=this.empresaList.filter(empresa=>empresa.correo==form.value.correo &&empresa.contrasenia==form.value.contrasenia);
+     //console.log(JSON.stringify(this.resultado));
+     localStorage.setItem('email',form.value.correo);
+     if(this.resultado[0]==null){
+       
+     console.log("array vacio");
+      alert('array vacio');}
+      else{
+        console.log(this.resultado[0]);
+        this.router.navigate(['/inicio']);
+        localStorage.setItem('email',form.value.correo);
+      }
+      
+     
   }
+     
 }
